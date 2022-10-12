8f9abf0f9758be0159157388aa881afbdd6d9702
Aman Dubey <aman@Amans-MacBook-Pro.local>
2022-10-12 23:22:55 +0530

    Started loopstudio project with basic screen and font custom settings

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily : {
+        sans : ['Josefin Sans', 'sans-serif'],
+        alata: ['Alata'],
+      }
+    },
+    screens : {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    }
+  },
+  plugins: [],
+}
